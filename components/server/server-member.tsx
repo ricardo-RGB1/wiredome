ed3cdf1e02a4448669e47374ebc43dbf691a611c
Ricardo Garcia Bahena <garci283@cougars.csusm.edu>
2025-02-06 09:35:51 -0800

    Fix: Disable ESLint rules causing deployment issues

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -1,4 +1,4 @@
-'use client'; 
+"use client";
 
 import { Member, MemberRole, Profile, Server } from "@prisma/client";
 import { ShieldCheck, ShieldAlert } from "lucide-react";
@@ -7,56 +7,49 @@ import { useRouter } from "next/navigation";
 import { UserAvatar } from "@/components/user-avatar";
 import { cn } from "@/lib/utils";
 
-// Server member will have the following properties; 
+// Server member will have the following properties;
 // This will be used to display the members in the server sidebar
 interface ServerMemberProps {
-    member: Member & { profile: Profile }; 
-    server: Server; 
+  member: Member & { profile: Profile };
 }
-
 
 const roleIconMap = {
-    [MemberRole.GUEST]: null, 
-    [MemberRole.MODERATOR]: <ShieldCheck className="size-4 ml-2 text-indigo-500" />, 
-    [MemberRole.ADMIN]: <ShieldAlert className="size-4 ml-2 text-rose-500" />, 
-}
+  [MemberRole.GUEST]: null,
+  [MemberRole.MODERATOR]: (
+    <ShieldCheck className="size-4 ml-2 text-indigo-500" />
+  ),
+  [MemberRole.ADMIN]: <ShieldAlert className="size-4 ml-2 text-rose-500" />,
+};
 
+export const ServerMember = ({ member }: ServerMemberProps) => {
+  const params = useParams();
+  const router = useRouter();
 
+  const icon = roleIconMap[member.role];
 
-export const ServerMember = ({
-    member,   
-    server,
-}: ServerMemberProps) => {
-    const params = useParams(); 
-    const router = useRouter(); 
- 
+  const handleMemberClick = () => {
+    router.push(`/servers/${params?.serverId}/conversations/${member.id}`);
+  };
 
-    const icon = roleIconMap[member.role]; 
-
-
-
-    const handleMemberClick = () => {
-        router.push(`/servers/${params?.serverId}/conversations/${member.id}`); 
-    }
-
-    return ( 
-      <button
-        onClick={handleMemberClick}
+  return (
+    <button
+      onClick={handleMemberClick}
+      className={cn(
+        "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
+        params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
+      )}
+    >
+      <UserAvatar src={member.profile.imageUrl} className="size-6 md:size-8" />
+      <p
         className={cn(
-            "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
-            params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700",
-        )}>
-        <UserAvatar 
-          src={member.profile.imageUrl}
-          className="size-6 md:size-8"
-        />
-        <p className={cn(
-            "font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
-            params?.memberId === member.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white"
-        )}>
-            {member.profile.name}
-        </p>
-        {icon}
-      </button>
-    )
-}
+          "font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
+          params?.memberId === member.id &&
+            "text-primary dark:text-zinc-200 dark:group-hover:text-white"
+        )}
+      >
+        {member.profile.name}
+      </p>
+      {icon}
+    </button>
+  );
+};
